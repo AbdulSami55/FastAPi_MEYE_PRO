6a5b032e0d72633082c43ad6f15b01a4eed6f115
JuanGar123 <JuanGar123>
2023-03-04 19:25:30 +0500

    done

--- VideoRecording.py
+++ VideoRecording.py
@@ -19,7 +19,7 @@ import ApiFunctions.TeacherSlots as apiteacherslots
 cout = 0
 
 class RTSPVideoWriterObject(object):
-    def __init__(self,ip, s, e, f,stime,etime,day,teacher_slot,teacher_id):
+    def __init__(self,ip, s, e, f,stime,etime,day,teacherName,timetableId,slotId):
         
         self.capture = cv2.VideoCapture(ip)
         self.s = s
@@ -28,13 +28,10 @@ class RTSPVideoWriterObject(object):
         self.st = stime
         self.et = etime
         self.day = day
-        self.slotid = teacher_slot.id
-        self.teacher_slot= teacher_slot
-        self.teacher_id = teacher_id
+        self.teacherName = teacherName
+        self.timetableId = timetableId
+        self.slotId = slotId
         self._key_lock = threading.Lock() 
-        self.fname = f'Recordings/file,{self.slotid},start_recording.mp4'
-        self.fcname = f'Recordings/file,{self.slotid},complete_recording.mp4'
-        self.fename = f'Recordings/file,{self.slotid},end_recording.mp4'
         self.codec = cv2.VideoWriter_fourcc(*'mp4v')
         self.frame_width = int(self.capture.get(3))
         self.frame_height = int(self.capture.get(4))
@@ -48,19 +45,25 @@ class RTSPVideoWriterObject(object):
         self.image_encodings=None
         self.teacherin = False
         self.teacherout = False
-        
+        self.tempFrameCount=0
+        self.tempFrame=None
+        self.readImageCount=0
+        self.status=False
+        self.ip=ip
         
     def update(self):
         lsttimein=[]
         lsttimeout=[]
         user_object =  apiuser.UserApi(user=muser)
-        user=  user_object.single_user_details(userid=self.teacher_id)
+        user=  user_object.single_user_details(teacherName=self.teacherName)
         userdata = user['data']
         image =  face_recognition.load_image_file(f'UserImages/Teacher/{userdata.image}')
         self.image_encodings = face_recognition.face_encodings(image)[0]
         while True:
+            self.capture = cv2.VideoCapture(self.ip)
             if self.capture.isOpened():
-                (self.status, self.frame) = self.capture.read()
+                if datetime.now().time()>self.st.time() and datetime.now().time()<self.et.time():
+                    (self.status, self.frame) = self.capture.read()     
                 if self.status and self.sc==0 and datetime.now().time()>self.st.time() and datetime.now().time()<self.et.time():
                     self.thread1 = threading.Thread(target=self.check_time, args=())
                     self.thread1.daemon = True
@@ -80,9 +83,17 @@ class RTSPVideoWriterObject(object):
                         self.totalteachertimeframes=0
                         self.teachertimeinframes=0
                         self.teachertimeoutframes=0
-                    self.thread2 = threading.Thread(target=self.check_time_using_facial_recognition, args=())
-                    self.thread2.daemon = True
-                    self.thread2.start()
+                    if self.tempFrameCount>30:
+                        if self._key_lock.locked():
+                            pass
+                        else:
+                            self.thread2 = threading.Thread(target=self.check_time_using_facial_recognition, args=())
+                            self.thread2.daemon = True
+                            self.thread2.start()
+                        #self.check_time_using_facial_recognition(tempFrame=tempFrame)
+                    else:
+                        self.tempFrameCount+=1
+                        
                     
                 if self.sc==1 and datetime.now().time()>self.et.time():
                     if self.teacherin==True :
@@ -95,6 +106,7 @@ class RTSPVideoWriterObject(object):
                     totaltimein =0
                     totaltimeout =0
                     overalltimemin=0
+                    totalsec=0
                     if lsttimein!=[]:  
                         if len(lsttimein)!=len(lsttimeout):
                             lsttimeout.append(datetime.now())
@@ -104,33 +116,58 @@ class RTSPVideoWriterObject(object):
                     overalltime = datetime.now() - self.st
                     sec = overalltime.total_seconds()
                     overalltimemin += int(sec / 60)
+                    
                     for (timein,timeout) in zip(lsttimein,lsttimeout):
                         time = timeout-timein
                         sec = time.total_seconds()
-                        totaltimein += int(sec / 60)
+                        totalsec += sec
+                    totalsec+=20
+                    if (float(totalsec/60)-int(totalsec/60)) > 0.5:
+                        totaltimein = int(totalsec/60) + 1
+                    else:
+                        totaltimein = int(totalsec/60)
                     totaltimeout = overalltimemin-totaltimein
                     checktime_object =  apichecktime.CheckTimeApi(checktime=mchecktime)
-                    ctime = mchecktime.CheckTime(id=0,teacherSlotID=self.slotid,totaltimein=totaltimein,totaltimeout=totaltimeout)
+                    ctime = mchecktime.CheckTime(id=0,teacherSlotID=self.slotId,totaltimein=totaltimein,totaltimeout=totaltimeout)
                     checktime_object.add_checktime(checktime=ctime)
-                    checktime =checktime_object.checksingletime_details(teacherSlotID=self.slotid)
+                    checktime =checktime_object.checksingletime_details(teacherSlotID=self.slotId)
                     checktimedata = checktime['data']
                     teacherslot_object = apiteacherslots.TeacherSlots(teacherslots=mteacherslots)
                     if totaltimein==0:
-                        self.teacher_slot.status = -1
-                        teacherslot_object.update_teacherslots_details(self.teacher_slot)
+                        teacherSlot = mteacherslots.TeacherSlot
+                        teacherSlot.id=0
+                        teacherSlot.timetableId=self.timetableId
+                        teacherSlot.status="Not Held"
+                        teacherSlot.slot = self.slotId
+                        teacherslot_object.update_teacherslots_details(teacherslots=teacherSlot)
                     else:
+                        if totaltimeout>20:
+                            teacherSlot = mteacherslots.TeacherSlot
+                            teacherSlot.id=0
+                            teacherSlot.timetableId=self.timetableId
+                            teacherSlot.status="Late + Held"
+                            teacherSlot.slot = self.slotId
+                            teacherslot_object.update_teacherslots_details(teacherslots=teacherSlot)
+                        else:
+                            teacherSlot = mteacherslots.TeacherSlot
+                            teacherSlot.id=0
+                            teacherSlot.timetableId=self.timetableId
+                            teacherSlot.status="Held"
+                            teacherSlot.slot = self.slotId
                         
-                        self.teacher_slot.status = 1
-                        teacherslot_object.update_teacherslots_details(self.teacher_slot)
+                            teacherslot_object.update_teacherslots_details(teacherslots=teacherSlot)
                     for (timein,timeout) in zip(lsttimein,lsttimeout):
                         checktimedetails_object =  apichecktimedetails.CheckTimeDetailsApi(checktimedetails=mchecktimedetails)
                         ctimedetails = mchecktimedetails.CheckTimeDetails(id=0,checkTimeID=checktimedata.id,timein=timein,timeout=timeout)
                         datetime.now()
                         checktimedetails_object.add_checktimedetails(checktimedetails=ctimedetails)   
                     self.sc=0
+                    self.readImageCount=0
             
                     
-
+   
+                
+        
     # def show_frame(self):
     #     if self.status:
     #         cv2image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGBA)
@@ -160,10 +197,11 @@ class RTSPVideoWriterObject(object):
             #     lmain1.after(1, self.show_frame())
 
     def start_recording(self):
+        fname = f'Recordings/file,{self.slotId},start_recording.mp4'
         rt = self.st + timedelta(seconds=60)
         print("start",self.st.time())
         print('record',rt.time())
-        start_video = cv2.VideoWriter(self.fname, self.codec, 30, (self.frame_width, self.frame_height))
+        start_video = cv2.VideoWriter(fname, self.codec, 30, (self.frame_width, self.frame_height))
         while True:
             start_video.write(self.frame)
             if datetime.now().time() > rt.time():
@@ -181,17 +219,18 @@ class RTSPVideoWriterObject(object):
                 start_video.release()
                 print("done start")
                 
-                recording = mrecordings.Recordings(id=0,teacherSlotID=self.slotid,filename=self.fname,date=str(datetime.now().date()))
+                recording = mrecordings.Recordings(id=0,teacherSlotID=self.slotId,filename=fname,date=str(datetime.now().date()))
                 rec_obj= apirecordings.RecordingsApi(recordings=mrecordings)
                 rec_obj.add_recordings(recordings=recording)
                 break
 
     def end_recording(self):
+        fename = f'Recordings/file,{self.slotId},end_recording.mp4'
         count = 1
         rt = self.et - timedelta(seconds=60)
         print("end", self.et.time())
         print('record', rt.time())
-        end_video = cv2.VideoWriter(self.fename, self.codec, 30, (self.frame_width, self.frame_height))
+        end_video = cv2.VideoWriter(fename, self.codec, 30, (self.frame_width, self.frame_height))
         while True:
             ct = datetime.now().time()
             while ct > rt.time() and ct<self.et.time():
@@ -201,19 +240,20 @@ class RTSPVideoWriterObject(object):
             if count == 0:
                 end_video.release()
                 print("done end")
-                recording = mrecordings.Recordings(id=0,teacherSlotID=self.slotid,filename=self.fename,date=str(datetime.now().date()))
+                recording = mrecordings.Recordings(id=0,teacherSlotID=self.slotId,filename=fename,date=str(datetime.now().date()))
                 rec_obj= apirecordings.RecordingsApi(recordings=mrecordings)
                 rec_obj.add_recordings(recordings=recording)
                 break
 
     def complete_recording(self):
-        complete_video = cv2.VideoWriter(self.fcname, self.codec, 30, (self.frame_width, self.frame_height))
+        fcname = f'Recordings/file,{self.slotId},complete_recording.mp4'
+        complete_video = cv2.VideoWriter(fcname, self.codec, 30, (self.frame_width, self.frame_height))
         while True:
             complete_video.write(self.frame)
             if datetime.now().time() > self.et.time():
                 complete_video.release()
                 print("done complete")
-                recording = mrecordings.Recordings(id=0,teacherSlotID=self.slotid,filename=self.fcname,date=str(datetime.now().date()))
+                recording = mrecordings.Recordings(id=0,teacherSlotID=self.slotId,filename=fcname,date=str(datetime.now().date()))
                 rec_obj= apirecordings.RecordingsApi(recordings=mrecordings)
                 rec_obj.add_recordings(recordings=recording)
                 break
@@ -235,7 +275,9 @@ class RTSPVideoWriterObject(object):
         #     break
     def check_time_using_facial_recognition(self):
         self._key_lock.acquire()
-        temp_image_path = f'temp{self.teacher_id}.JPG'
+    
+        self.tempFrameCount=0
+        temp_image_path = f'temp{self.timetableId}.JPG'
         cv2.imwrite(temp_image_path,self.frame)
         test_image =  face_recognition.load_image_file(temp_image_path)
         face_locations = face_recognition.face_locations(test_image)
@@ -255,12 +297,12 @@ class RTSPVideoWriterObject(object):
         self.totalteachertimeframes+=1
         self._key_lock.release()
         print(f'''
-             Total Frames={self.totalteachertimeframes}
-             Time In Frames={self.teachertimeinframes}
-             Time Out Frames={self.teachertimeoutframes}
-              ''')
-       
-        
+            Total Frames={self.totalteachertimeframes}
+            Time In Frames={self.teachertimeinframes}
+            Time Out Frames={self.teachertimeoutframes}
+            ''')
+    
+    
        
 
 
